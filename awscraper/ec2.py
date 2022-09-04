ba3148e54a73e6a61d98a91ea7b9969a9d29c69f
Rob Eden <rob.eden@forcepointgov.com>
2022-09-04 17:10:51 -0500

    EC2 instance parsing

---
+++ awscraper/ec2.py
@@ -0,0 +1,58 @@
+import boto3
+import datetime
+from dataclasses import dataclass
+from enum import IntEnum
+from typing import Optional, Collection, Dict
+
+from awscraper import types
+
+
+class InstanceState(IntEnum):
+    # https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_InstanceState.html
+    PENDING = 0
+    RUNNING = 16
+    SHUTTING_DOWN = 32
+    TERMINATED = 48
+    STOPPING = 64
+    STOPPED = 80
+
+
+@dataclass
+class InstanceInfo(types.IdentifiedResource):
+    type: str
+    state: InstanceState
+    image_id: str
+    private_ip: str
+    public_ip: Optional[str]
+    subnet_id: str
+    vpc_id: str
+    arch: str
+    reservation_id: str
+    volume_ids: Collection[str]
+    launch_time: datetime.datetime
+
+
+def gather(session: boto3.Session) -> Dict[str, InstanceInfo]:
+    ec2_client = session.client("ec2")
+    instance_map: Dict[str, InstanceInfo] = {}
+    for page in ec2_client.get_paginator('describe_instances').paginate():
+        reservations = page['Reservations']
+        for reservation in reservations:
+            instances = reservation['Instances']
+            for i in instances:
+                instance_map[i['InstanceId']] = InstanceInfo(
+                    i['InstanceId'],
+                    types.parse_tags(i.get('Tags')),
+                    i['InstanceType'],
+                    i['InstanceId'],
+                    i['ImageId'],
+                    i['PrivateIpAddress'],
+                    i.get('PublicIpAddress'),
+                    i['SubnetId'],
+                    i['VpcId'],
+                    i['Architecture'],
+                    reservation['ReservationId'],
+                    [bdm['VolumeId'] for bdm in i.get("Ebs", {}).get("BlockDeviceMappings", [])],
+                    i['LaunchTime']
+                )
+    return instance_map
--- awscraper/main.py
+++ awscraper/main.py
@@ -1,16 +1,55 @@
+import os
+from typing import List, Optional
+
 import boto3
+from botocore.exceptions import ClientError
 
-
-ec2_client = boto3.client("ec2")
-sts_client = boto3.client("sts")
+from awscraper import ec2
 
 if __name__ == '__main__':
+    session = boto3.Session(profile_name='548094779648_ps-p-account-admin')
+    sts_client = session.client("sts")
     caller = sts_client.get_caller_identity()
     print(f"Caller: user={caller.get('UserId')} account={caller.get('Account')}")
 
-    regions = [r.get("RegionName") for r in ec2_client.describe_regions(AllRegions=False).get("Regions")]
-    print(f"Regions: {regions}")
-
+    # Note: session.get_available_regions("ec2") includes regions which are not enabled, so only
+    #       using this to get the region to jump from.
+    regions = session.get_available_regions("ec2")
     is_gov = regions[0].startswith("us-gov")
 
-    
+    # Note: session.get_available_regions("ec2") includes regions which are not enabled
+    test_region = "us-gov-east-1" if is_gov else "us-east-1"
+    regions = [r["RegionName"]
+               for r in session.client("ec2", region_name=test_region).describe_regions()["Regions"]
+               if r["OptInStatus"] != "not-opted-in"]
+    print(f"Regions: {regions}")
+
+    region_session = None
+    partition: Optional[str] = None   # aws, aws-cn, aws-us-gov, aws-iso, aws-iso-b
+    for region in regions:
+        # TODO: REMOVE!
+        if region != "us-east-1":
+            continue
+
+        region_session = boto3.Session(profile_name='548094779648_ps-p-account-admin', region_name=region)
+
+        regional_sts = region_session.client("sts")
+        # The point of this call is check if access to the region is enabled
+        try:
+            regional_sts.get_caller_identity()
+        except ClientError as e:
+            print(f"Skipping region: {region}")
+            continue
+
+        if partition is None:
+            partition = region_session.get_partition_for_region(region)
+            print(f"Partition: {partition}")
+
+        region_printed = False
+        ec2_client = region_session.client("ec2")
+        instance_map = ec2.gather(region_session)
+
+        print(f"{region}: instances={len(instance_map)}")
+        if instance_map:
+            for k, v in instance_map.items():
+                print(f"  {v}")
